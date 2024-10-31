35d874271cbbd36c297e710857c2517504e54003
afterrest <afterrest00@gmail.com>
2024-10-31 23:29:41 +0900

    문제 해결-java ee 9.1 -> 8로 변경

--- src/main/java/org/zerock/jdbcex/controller/TodoListController.java
+++ src/main/java/org/zerock/jdbcex/controller/TodoListController.java
@@ -1,10 +1,10 @@
 package org.zerock.jdbcex.controller;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.zerock.jdbcex.dto.TodoDTO;
 import org.zerock.jdbcex.service.TodoService;
