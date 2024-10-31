81baa8b70d1b7e9faee3a9edd3970e6ad62e67df
afterrest <afterrest00@gmail.com>
2024-10-31 22:49:36 +0900

    2.3 -half

---
+++ src/main/java/org/zerock/jdbcex/service/TodoService.java
@@ -0,0 +1,31 @@
+package org.zerock.jdbcex.service;
+
+import org.modelmapper.ModelMapper;
+import org.zerock.jdbcex.dao.TodoDAO;
+import org.zerock.jdbcex.domain.TodoVO;
+import org.zerock.jdbcex.dto.TodoDTO;
+import org.zerock.jdbcex.util.MapperUtil;
+
+import java.util.List;
+
+public enum TodoService {
+
+    INSTANCE;
+
+    private TodoDAO dao;
+    private ModelMapper modelMapper;
+
+    TodoService() {
+        dao = new TodoDAO();
+        modelMapper = MapperUtil.INSTANCE.get();
+    }
+
+    public void register(TodoDTO todoDTO) throws Exception {
+        TodoVO todoVO = modelMapper.map(todoDTO, TodoVO.class);
+        System.out.println("todoVO: "+todoVO);
+        dao.insert(todoVO);
+    }
+
+    /*public List<TodoDTO> listAll() {
+    }*/
+}
